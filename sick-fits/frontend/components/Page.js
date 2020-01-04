ad4efdd61b3645e73ba9838ff8cd25f49ff1fc5a
Matt Wong <mattwong59@gmail.com>
2020-01-03 20:45:39 -0800

    Add meta tags

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Meta from "./Meta";
 
 export default class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         <p>Page Component</p>
         {this.props.children}
